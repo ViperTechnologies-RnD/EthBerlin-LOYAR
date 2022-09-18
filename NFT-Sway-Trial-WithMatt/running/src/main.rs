45de59be4043721f55d3c69a4ce2b744cd03f7fd
FrankBevr <frank_dierolf@web.de>
2022-09-18 11:56:24 +0200

    ./NFT-Sway-Trial-WithMatt added

---
+++ NFT-Sway-Trial-WithMatt/running/src/main.rs
@@ -0,0 +1,28 @@
+use fuels::prelude::*;
+use fuels::signers::fuel_crypto::SecretKey;
+use std::str::FromStr;
+
+fn main() {
+    running();
+}
+
+async fn running(){
+    // Create a provider pointing to the testnet.
+    let provider = Provider::connect("node-beta-1.fuel.network").await.unwrap();
+
+    let phrase = "two task reflect transfer club shy home dragon guilt upon left zone bridge guitar fuel shaft what noodle over scatter buffalo dress unaware force";
+
+    // Create first account from mnemonic phrase.
+    let wallet = WalletUnlocked::new_from_mnemonic_phrase_with_path(
+        phrase,
+        Some(provider.clone()),
+        "m/44'/1179993420'/0'/0/0",
+    ).unwrap();
+
+    // Create the wallet
+    // let wallet = WalletUnlocked::new_from_private_key(secret, Some(provider));
+
+    // Get the wallet address. Used later with the faucet
+    dbg!(wallet.address().to_string());
+
+}
